5ce2c8c593841c8d955a263669c7f7151de59e4a
Abdramanov Kuanysh <abdramanovkuanys@gmail.com>
2024-07-24 20:09:18 +0500

    final video FIX

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,56 @@
-
-
 import 'dart:io';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:oqu_way/presentation/application.dart';
-
 import 'config/app_navigation.dart';
-import 'firebase_options.dart';
 
-void main() async{
+// class CustomHttpOverrides extends HttpOverrides {
+//   final HttpClient _client;
+//
+//   CustomHttpOverrides(this._client);
+//
+//   @override
+//   HttpClient createHttpClient(SecurityContext? context) {
+//     return _client;
+//   }
+// }
+
+class CustomHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    final client = super.createHttpClient(context);
+    client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+    return client;
+  }
+}
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  ByteData data =
-  await PlatformAssetBundle().load('assets/certificates/oqutest_kz.crt');
-  SecurityContext.defaultContext
-      .setTrustedCertificatesBytes(data.buffer.asUint8List());
-  ByteData secondCertData =
-  await PlatformAssetBundle().load('assets/certificates/_oquway_kz.crt');
-  SecurityContext.defaultContext
-      .setTrustedCertificatesBytes(secondCertData.buffer.asUint8List());
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  try {
+
+    // ByteData data = await rootBundle.load('assets/certificates/oqutest_kz.crt');
+    // ByteData secondCertData = await rootBundle.load('assets/certificates/_oquway_kz.crt');
+    //
+    // SecurityContext context = SecurityContext.defaultContext;
+    // context.setTrustedCertificatesBytes(secondCertData.buffer.asUint8List());
+    // context.setTrustedCertificatesBytes(data.buffer.asUint8List());
+    //
+    // HttpClient customClient = HttpClient(context: context);
+
+    HttpOverrides.global = CustomHttpOverrides();
+
+  } catch (e) {
+
+    print('Error loading certificates: $e');
+  }
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+
+  await Firebase.initializeApp();
 
   AppNavigation.getStatus();
 
--- lib/presentation/screens/course_screen/course_videos.dart
+++ lib/presentation/screens/course_screen/course_videos.dart
@@ -1,23 +1,16 @@
-import 'dart:io';
-
-import 'package:dio/dio.dart';
 import 'package:flick_video_player/flick_video_player.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_connect/http/src/request/request.dart';
 import 'package:go_router/go_router.dart';
 import 'package:oqu_way/config/app_text.dart';
 import 'package:oqu_way/data/local/shared_preferences_operator.dart';
-import 'package:oqu_way/util/custom_exeption.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as path;
 import 'package:video_player/video_player.dart';
 
-import '../../../config/app_api_endpoints.dart';
 import '../../../config/app_colors.dart';
 import '../../../domain/media_file.dart';
 
 class CourseVideos extends StatefulWidget {
-  const CourseVideos({super.key, required this.description, required this.file});
+  const CourseVideos(
+      {super.key, required this.description, required this.file});
 
   final String description;
   final MediaFile? file;
@@ -27,47 +20,22 @@ class CourseVideos extends StatefulWidget {
 }
 
 class _CourseVideosState extends State<CourseVideos> {
-
   FlickManager? flickManager;
 
-  String? videoFilePath;
+  String? videoUrl = '';
 
   Future<void> getVideo(String fileId) async {
-    final Dio dio = Dio();
-
     try {
       String? token = await SharedPreferencesOperator.getAccessToken();
 
-      dio.options.headers['Authorization'] = 'Bearer $token';
-
-      final response = await dio.get<ResponseBody>(
-        '${AppApiEndpoints.getVideo}$fileId',
-        options: Options(responseType: ResponseType.stream),
-      );
-
-      if (response.data != null) {
-
-        print(response.data!.stream);
-
-        final stream = response.data!.stream;
-
-        // Create a temporary file to store video data
-        final tempDir = await getTemporaryDirectory();
-        final filePath = path.join(tempDir.path, 'video.mp4');
-        final file = File(filePath);
-
-        // Write the video data to the file
-        await file.writeAsBytes(await stream.toBytes());
-
-        setState(() {
-          videoFilePath = filePath;
-          flickManager = FlickManager(
-            videoPlayerController: VideoPlayerController.file(file),
-          );
-        });
-      }
+      setState(() {
+        flickManager = FlickManager(
+          videoPlayerController: VideoPlayerController.networkUrl(Uri.parse(
+              'https://api.oquway.kz/api/media/video/$fileId?access_token=Bearer $token')),
+        );
+      });
     } catch (e) {
-      rethrow;
+      print('Failed to get video: $e');
     }
   }
 
@@ -82,13 +50,6 @@ class _CourseVideosState extends State<CourseVideos> {
   @override
   void dispose() {
     flickManager?.dispose();
-    if (videoFilePath != null) {
-      final file = File(videoFilePath!);
-      if (file.existsSync()) {
-        file.deleteSync();
-        print('deleted');
-      }
-    }
     super.dispose();
   }
 
@@ -109,11 +70,11 @@ class _CourseVideosState extends State<CourseVideos> {
                   },
                   child: Text(
                     '<  ${AppText.video}',
-                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
                   ),
                 ),
                 const SizedBox(height: 20),
-
                 Expanded(
                   child: SingleChildScrollView(
                     child: Column(
@@ -121,22 +82,28 @@ class _CourseVideosState extends State<CourseVideos> {
                       children: [
                         if (flickManager != null)
                           ClipRRect(
-                            borderRadius: const BorderRadius.all(Radius.circular(10)),
-                            child: FlickVideoPlayer(flickManager: flickManager!),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                            child:
+                                FlickVideoPlayer(flickManager: flickManager!),
                           )
                         else
                           Container(
                             height: 200,
                             decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
                               color: Colors.black,
                             ),
-                            child: Center(child: CircularProgressIndicator(color: AppColors.blueColor)),
+                            child: Center(
+                                child: CircularProgressIndicator(
+                                    color: AppColors.blueColor)),
                           ),
                         const SizedBox(height: 36),
                         Text(
                           AppText.description,
-                          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          style: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
                         ),
                         const SizedBox(height: 20),
                         Text(
