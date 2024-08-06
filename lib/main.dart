00afded452a9b9888ce93719081ba514c9e25e96
Abdramanov Kuanysh <abdramanovkuanys@gmail.com>
2024-08-06 11:10:10 +0500

    certificate uncomment

--- lib/main.dart
+++ lib/main.dart
@@ -5,41 +5,41 @@ import 'package:flutter/services.dart';
 import 'package:oqu_way/presentation/application.dart';
 import 'config/app_navigation.dart';
 
-// class CustomHttpOverrides extends HttpOverrides {
-//   final HttpClient _client;
-//
-//   CustomHttpOverrides(this._client);
-//
-//   @override
-//   HttpClient createHttpClient(SecurityContext? context) {
-//     return _client;
-//   }
-// }
-
 class CustomHttpOverrides extends HttpOverrides {
+  final HttpClient _client;
+
+  CustomHttpOverrides(this._client);
+
   @override
   HttpClient createHttpClient(SecurityContext? context) {
-    final client = super.createHttpClient(context);
-    client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
-    return client;
+    return _client;
   }
 }
+
+// class CustomHttpOverrides extends HttpOverrides {
+//   @override
+//   HttpClient createHttpClient(SecurityContext? context) {
+//     final client = super.createHttpClient(context);
+//     client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+//     return client;
+//   }
+// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   try {
 
-    // ByteData data = await rootBundle.load('assets/certificates/oqutest_kz.crt');
-    // ByteData secondCertData = await rootBundle.load('assets/certificates/_oquway_kz.crt');
-    //
-    // SecurityContext context = SecurityContext.defaultContext;
-    // context.setTrustedCertificatesBytes(secondCertData.buffer.asUint8List());
-    // context.setTrustedCertificatesBytes(data.buffer.asUint8List());
-    //
-    // HttpClient customClient = HttpClient(context: context);
+    ByteData data = await rootBundle.load('assets/certificates/oqutest_kz.crt');
+    ByteData secondCertData = await rootBundle.load('assets/certificates/_oquway_kz.crt');
 
-    HttpOverrides.global = CustomHttpOverrides();
+    SecurityContext context = SecurityContext.defaultContext;
+    context.setTrustedCertificatesBytes(secondCertData.buffer.asUint8List());
+    context.setTrustedCertificatesBytes(data.buffer.asUint8List());
+
+    HttpClient customClient = HttpClient(context: context);
+
+    HttpOverrides.global = CustomHttpOverrides(customClient);
 
   } catch (e) {
 
